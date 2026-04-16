89053978395e3252945ac5d8d5600d7354e656ea
Lorgio09 <lorgio.choque09@gmail.com>
2026-04-15 21:47:10 -0400

    Flujo de recuperacion de contraseña

--- config/urls.py
+++ config/urls.py
@@ -27,6 +27,12 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
+    # Recuperación de contraseña
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"), name="password_reset_complete"),
+
     # Dashboard y Vistas Principales
     path('', views.dashboard, name='home'),
     path('dashboard/', views.dashboard, name='dashboard_general'),
