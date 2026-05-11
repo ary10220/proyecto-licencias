cea65ee1575ba2a54811c326abd3bfe08d9500ae
Jose A. GUevara <jose.guevara1caballero@gmail.com>
2026-05-11 15:25:40 -0400

    Mejora filtros dinamicos en gestion global

--- gestion_global/static/gestion_global/js/filters.js
+++ gestion_global/static/gestion_global/js/filters.js
@@ -10,9 +10,84 @@
         var search = form.querySelector('input[type="search"]');
         var selects = form.querySelectorAll('select');
         var timer = null;
+        var controller = null;
+
+        function getTableCard() {
+            return document.querySelector('.gg-table-card');
+        }
+
+        function buildUrl() {
+            var data = new FormData(form);
+            var params = new URLSearchParams();
+            data.forEach(function (value, key) {
+                value = String(value || '').trim();
+                if (value) {
+                    params.set(key, value);
+                }
+            });
+            return form.action.split('?')[0] + (params.toString() ? '?' + params.toString() : '');
+        }
+
+        function replaceTableFrom(html) {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(html, 'text/html');
+            var nextCard = doc.querySelector('.gg-table-card');
+            var currentCard = getTableCard();
+
+            if (!nextCard || !currentCard) {
+                window.location.href = buildUrl();
+                return;
+            }
+
+            currentCard.replaceWith(nextCard);
+            nextCard.classList.add('is-updated');
+            window.setTimeout(function () {
+                nextCard.classList.remove('is-updated');
+            }, 260);
+        }
 
         function submitFilter() {
-            form.requestSubmit ? form.requestSubmit() : form.submit();
+            if (!window.fetch || !window.DOMParser || !window.history) {
+                form.requestSubmit ? form.requestSubmit() : form.submit();
+                return;
+            }
+
+            var url = buildUrl();
+            var card = getTableCard();
+            if (controller) {
+                controller.abort();
+            }
+            controller = new AbortController();
+
+            if (card) {
+                card.classList.add('is-loading');
+            }
+
+            fetch(url, {
+                headers: { 'X-Requested-With': 'XMLHttpRequest' },
+                signal: controller.signal
+            })
+                .then(function (response) {
+                    if (!response.ok) {
+                        throw new Error('HTTP ' + response.status);
+                    }
+                    return response.text();
+                })
+                .then(function (html) {
+                    replaceTableFrom(html);
+                    window.history.replaceState({}, '', url);
+                })
+                .catch(function (error) {
+                    if (error.name !== 'AbortError') {
+                        form.requestSubmit ? form.requestSubmit() : form.submit();
+                    }
+                })
+                .finally(function () {
+                    var freshCard = getTableCard();
+                    if (freshCard) {
+                        freshCard.classList.remove('is-loading');
+                    }
+                });
         }
 
         function submitSoon() {
